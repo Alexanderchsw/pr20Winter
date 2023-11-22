c75b4100beee3771abae7f1b41113cd59773d730
alexw <alexwins570975@gmail.com>
2023-11-22 19:18:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pr20winter/Constant.java
@@ -0,0 +1,7 @@
+package com.example.pr20winter;
+
+public class Constant {
+    public final static String USER_NAME = "user_name";
+    public final static String USER_S_NAME = "user_s_name";
+    public final static String USER_EMAIL = "user_email";
+}
